d71a9895365d44ee7023efd9bd5c7ae4551169b4
ChristianHurtado29 <christianhurtado@pursuit.org>
2019-11-14 19:00:05 -0500

    finishing ui

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -8,12 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var hangmanView: UIView!
+    @IBOutlet weak var headingLabel: UILabel!
+    @IBOutlet weak var textfieldLabel: UILabel!
+    
+    var turnPlay = 0
+    
+    var status:GameStatus = .hint
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    myTextField.delegate = self
+    hintLabel.text = ""
+    headingLabel.text = "Player 1's turn"
+    textfieldLabel.text = "Enter hint: "
+    
+    //let textField = UITextField()
+    //textField.isSecureTextEntry = true // secure text
   }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if status == GameStatus.game {
+        let strCount = string.count
+        let textCount = textField.text?.count ?? 0
+        if strCount + textCount > 1 {
+            return false
+        }
+        return true
+        }
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        switch status {
+        case .hint:
+            hintLabel.text = "Hint: \(myTextField.text ?? "")"
+            myTextField.text = ""
+            status = .word
+            textfieldLabel.text = "Enter word:"
+            textField.isSecureTextEntry = true
+        case .word:
+            textfieldLabel.text = "Guess letter"
+            textField.isSecureTextEntry = false
+            status = .game
+            
+        case .game:
+            
+            print("game")
+        }
+        myTextField.text = ""
+        return true
+    }
+
 
 
 }
---
+++ Hangman/Hangman/gameStatus.swift
@@ -0,0 +1,13 @@
+//
+//  gameStatus.swift
+//  Hangman
+//
+//  Created by Christian Hurtado on 11/14/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+enum GameStatus {
+    case hint, word, game
+}
